904b5a7d2c08de7e75565331dcc166ed52897c55
flo <hopemagie@gmail.com>
2024-01-22 17:36:52 +0100

    deploy supervised model on cloud

--- api/app.py
+++ api/app.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
 import numpy as np
+import requests
 from joblib import load
 from pydantic import BaseModel
 from api.preprocessing import preprocess_text
+import streamlit as st
+import httpx
 
 app = FastAPI()
+mlb_model_url = 'https://github.com/walterwhites/Categorisez-automatiquement-des-questions/releases/download/v1.0.0/model_supervised_MultinomialNB.joblib'
+oneVsRestClassifier_mlb_model_url = 'https://github.com/walterwhites/Categorisez-automatiquement-des-questions/releases/download/v1.0.0/model_supervised_OneVsRestClassifier_MultinomialNB.joblib'
 
+combined_pipeline = None
+mlb = None
+
+api_url = "https://share.streamlit.io/votre-nom/nom-de-votre-application/models/supervised/predict/"
+
+def download_file(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as f:
+        f.write(response.content)
 @app.on_event("startup")
 def load_models():
     global combined_pipeline
     global mlb
-    combined_pipeline = load('models/model_supervised_OneVsRestClassifier_MultinomialNB.joblib')
-    mlb = load('models/model_supervised_MultinomialNB.joblib')
+
+    download_file(mlb_model_url, "mlb_model.joblib")
+    download_file(oneVsRestClassifier_mlb_model_url, "oneVsRestClassifier_mlb_model.joblib")
+    combined_pipeline = load('oneVsRestClassifier_mlb_model.joblib')
+    mlb = load('mlb_model.joblib')
 
 class PredictionResponse(BaseModel):
     prediction: str
@@ -40,8 +58,14 @@ def supervised_predict(title: str, body: str):
         print("\n")
 
     return {"prediction":  list(zip(top_tags_combined_sorted, top_classes_probabilities_sorted))}
+def main():
+    st.title("Votre Application Streamlit")
+    title = st.text_input("Titre")
+    body = st.text_area("Corps")
 
-@app.post("/predict/", response_model=PredictionResponse)
-def predict(question: str):
-    prediction = combined_pipeline.predict([question])
-    return PredictionResponse(prediction=prediction)
+    if st.button("Prédire"):
+        with st.spinner("En cours de prédiction..."):
+            data = {"title": title, "body": body}
+            response = requests.post(api_url, json=data)
+            prediction = response.json()
+            st.write("Prédiction:", prediction)
--- models/README.md
+++ models/README.md
@@ -1,6 +1,6 @@
 # Cat-gorisez-automatiquement-des-questions
 
-## récupérer les données du dataset
+## Récupérer les données du dataset
 https://data.stackexchange.com/stackoverflow/query/edit/1810083
 ```SQL
 SELECT TOP 50000 Title, Body, Tags, Score
@@ -8,3 +8,17 @@ FROM Posts
 WHERE PostTypeId = 1 AND ViewCount >= 20 AND AnswerCount >= 1
 AND Score >= 5 AND LEN(Tags) - LEN(REPLACE(Tags, '<','')) >= 5
 ```
+
+## mlflow UI
+Dans la racine du projet lancez la commande
+```
+mlflow ui
+```
+
+## API en local
+Dans la racine du projet lancez la commande
+```
+ uvicorn api.app:app --reload
+```
+
+Ensuite, naviguez vers http://localhost:8000/docs
