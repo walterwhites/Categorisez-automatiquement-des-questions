873b2eafb0d11ce1a0b5c08a6bbfc1825f259c0f
flo <hopemagie@gmail.com>
2024-01-23 01:06:19 +0100

    start with vercel

--- api/steamlit.py
+++ api/steamlit.py
@@ -3,6 +3,7 @@ import streamlit as st
 import subprocess
 
 api_url = "https://categorisez-automatiquement-des-questions-5k9epyoc4gkbruabktk8.streamlit.app/models/supervised/predict/"
+
 def main():
     st.title("Votre Application Streamlit")
     title = st.text_input("Titre")
@@ -15,9 +16,5 @@ def main():
             prediction = response.json()
             st.write("Prédiction:", prediction)
 
-def start_fastapi():
-    cmd = "uvicorn api.app:app --host 0.0.0.0 --port 8080 --reload"
-    subprocess.Popen(cmd, shell=True)
-
-start_fastapi()
-main()
+if __name__ == "__main__":
+    main()
